10dcd6725c1694ad88894cd600542cffa429d596
dong <eb61010908@163.com>
2023-08-31 16:59:37 +0800

    增加gsap动画测试

---
+++ Html1/JavaScript/gsapTest_1.js
@@ -0,0 +1,21 @@
+const tl = gsap.timeline({ repeat:2 , yoyo:true });
+
+tl.from( ".ball" , {
+    duration: 2,
+    // y : () => Math.random() * -300 + 200 ,
+    delay: .5,
+    y: "random(-200 , 200)",
+    opacity: 0,
+    stagger: 0.25,
+} )
+
+//获取
+// var myObject = {rotation: 0};
+// gsap.to(myObject , {
+//     duration: 20,
+//     rotation: 360,
+//     // onUpdate: function() {
+//     //     console.log(myObject.rotation);
+//     // }
+// })
+
